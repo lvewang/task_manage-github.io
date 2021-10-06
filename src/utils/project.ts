53ec11d4a10efa5326bf25ec7d433b394d574f7f
Qunling Wang <qunlingwang@gmail.com>
2021-10-05 20:19:24 -0700

    feat: try optimistic update

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -5,6 +5,7 @@ import {
   useQueryClient,
 } from "react-query";
 import { Project } from "screens/ProjectListScreen/List";
+import { useProjectsSearchParams } from "screens/ProjectListScreen/util";
 import { useHttp } from "utils/http";
 import { useAsync } from "utils/useAsync";
 
@@ -18,11 +19,30 @@ export const useProjects = (param?: Partial<Project>) => {
 export const useEditProject = () => {
   const client = useHttp();
   const queryClient = useQueryClient();
+  const [searchParams] = useProjectsSearchParams();
+  const queryKey = ["projects", searchParams];
   return useMutation(
     (params: Partial<Project>) =>
       client(`projects/${params.id}`, { method: "PATCH", data: params }),
     {
-      onSuccess: () => queryClient.invalidateQueries("projects"),
+      onSuccess: () => queryClient.invalidateQueries(queryKey),
+      onMutate: async (target) => {
+        const previousItems = queryClient.getQueryData(queryKey);
+        queryClient.setQueryData(queryKey, (old?: Project[]) => {
+          return (
+            old?.map((project) =>
+              project.id === target.id ? { ...project, ...target } : project
+            ) || []
+          );
+        });
+        return previousItems;
+      },
+      onError: (error, newItem, context) => {
+        queryClient.setQueryData(
+          queryKey,
+          (context as { previousItems: Project[] }).previousItems
+        );
+      },
     }
   );
 };
