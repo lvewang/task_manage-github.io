c29f0e4613785324e646c38a84ca47625e2c8cd2
Qunling Wang <qunlingwang@gmail.com>
2021-11-03 22:21:09 -0700

    Updates

--- src/utils/task-type.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "react-query";
-import { TaskType } from "types/task-type";
-import { useHttp } from "utils/http";
-
-export const useTaskTypes = () => {
-  const client = useHttp();
-  return useQuery<TaskType[]>(["taskTypes"], () => client("taskTypes"));
-};
